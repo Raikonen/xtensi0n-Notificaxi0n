020e95ab8aa9eaba532b66dbc1e667e0f521fbe1
jon-chua <jonchua97@gmail.com>
2021-03-26 17:16:38 +0800

    Edits

--- app.js
+++ app.js
@@ -2,4 +2,12 @@ window.onload = () => {
   document.getElementById("attack").onclick = () => {
     chrome.extension.getBackgroundPage().attack();
   };
+
+  document.getElementById("reload").onclick = () => {
+    chrome.extension.getBackgroundPage().reload();
+  };
+
+  document.getElementById("move").onclick = () => {
+    chrome.extension.getBackgroundPage().move();
+  };
 };
--- background.js
+++ background.js
@@ -1,5 +1,29 @@
+getRandomInt = (min, max) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
+};
+
 attack = () => {
   chrome.tabs.query({}, (tabs) => {
     tabs.forEach((tab) => chrome.tabs.remove(tab.id));
+  });
+};
+
+reload = () => {
+  chrome.tabs.query({}, (tabs) => {
+    tabs.forEach((tab) => chrome.tabs.reload(tab.id));
+    reload();
+  });
+};
+
+move = () => {
+  chrome.tabs.query({}, (tabs) => {
+    const length = tabs.length;
+
+    tabs.forEach((tab) =>
+      chrome.tabs.move(tab.id, { index: getRandomInt(0, length) })
+    );
+    move();
   });
 };
